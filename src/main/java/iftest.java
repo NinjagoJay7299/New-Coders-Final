2625ccf18e0df3540d76f9e315f91d3d5d578034
NinjagoJay7299 <attilabloom7299@gmail.com>
2026-01-11 15:29:41 -0500

    added if statement test

---
+++ src/main/java/iftest.java
@@ -0,0 +1,16 @@
+package main.java;
+// utilize your knowledge of if statement such that you cna calculate if you have enough food for you and your dogs to survive 
+public class iftest {
+    boolean a = false;
+    boolean b = true;
+    String apple = "apple";
+    int numDogs = 4;
+    int foodCost = 13;
+    int income = 36;
+    int foodPerDog = 2;
+    int numHumans = 6;
+    boolean survive;
+
+    //Ignore this
+    public iftest(){}
+}
