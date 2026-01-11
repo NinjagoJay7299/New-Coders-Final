b4baa065658b82e5aecdae9570c2e9f6af65bf8f
NinjagoJay7299 <attilabloom7299@gmail.com>
2026-01-11 12:35:05 -0500

    added a data types test

---
+++ src/main/java/Data.java
@@ -0,0 +1,14 @@
+package main.java;
+// fix the data types on the given variable to match the values 
+public class Data {
+    public void data(){
+       var = 9;
+       2ndVar = "that thingy"; 
+       3rdVar = 'a';
+       4thVar = true;
+       5thVar = false;
+       6thVar = 6.8;
+       System.out.println(var, 2ndVar, 3rdVar, 4thVar, 5thVar, 6thVar);
+    }
+    
+}
