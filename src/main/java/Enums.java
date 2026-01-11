efa4012ba5e3969407af7ad9633630920dd40a97
NinjagoJay7299 <attilabloom7299@gmail.com>
2026-01-11 12:36:28 -0500

    updated enums to no longer have answers

--- src/main/java/Enums.java
+++ src/main/java/Enums.java
@@ -8,14 +8,8 @@ public class Enums {
         Tuesday,
         Wednesday
     }
-    public enum Flavor {
-        Chocolate,
-        Vanilla
-    }
 
     public void Check(Day day, Flavor flavor) {
-        if (day == Day.Friday && flavor == Flavor.Chocolate) {
-            System.out.println("It's Friday and the flavor is Chocolate!");
-        }
+        
     }
 }
